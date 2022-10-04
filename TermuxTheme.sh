2bda59e12f62ec243fe149bbc112ae9c3b2dcd64
User Eror21 <104538928+USER2196@users.noreply.github.com>
2022-10-04 22:26:18 +0700

    Add files via upload

---
+++ TermuxTheme.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+clear
+sleep 0.5
+echo ""
+echo "  [VISIT https://bajetech.org TO LEARN MORE]"|lolcat
+echo "    _____   _____ _   _ _____ __  __ _____"|lolcat
+echo "   |_   _| |_   _| | | | ____|  \/  | ____|"|lolcat
+echo "     | |_____| | | |_| |  _| | |\/| |  _|"  |lolcat
+echo "     | |_____| | |  _  | |___| |  | | |___" |lolcat
+echo "     |_|     |_| |_| |_|_____|_|  |_|_____| V1.5"|lolcat
+echo "  [Modifikasi BY: User Eror21]"|lolcat
+sleep 0.5
+echo ""
+echo "You Are About To Switch Termux To Colorfully Mode."|lolcat
+read -p 'Klik Enter Untuk Mulai atau Klik CTRL + Z Untuk Kembali'
+echo ""
+echo ""
+sleep 0.5
+echo "Ok Tema Termux Berhasil Di Pasang"|lolcat
+sleep 2.0
+echo " Silahkan Tunnggu 10 detik Untuk Menginstal Bahan..."|lolcat
+sleep 2.0
+cd
+cd /$HOME
+rm //data/data/com.termux/files/usr/etc/bash.bashrc
+cd
+mv /data/data/com.termux/files/home/TermuxTheme/code/bash.bashrc //data/data/com.termux/files/usr/etc/bash.bashrc
+sleep 8.0
+echo "Selesai, Silahkan Lihat Hasilnya Yang Sangan Keren"|lolcat
+sleep 1.0
+echo ""
+echo ""
+echo "SC INI SUDAH DI RECOD ULANG MENGGUNAKAN BAHASA INDONESIA"|lolcat
+echo "Silahkan Di subscribe Channel User Eror21 "|lolcat
